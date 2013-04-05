af9396ecadcc383028788aff53da0f72f42025a2
Dan Heidel <dan.heidel@gmail.com>
2013-04-05 12:02:23 -0700

    Finished changes to add gamma correction and some various cleanup.

--- testleds_ada.ino
+++ testleds_ada.ino
@@ -1,6 +1,5 @@
 #include <SPI.h>
 #include <Adafruit_WS2801.h>
-#include <MHV_WS2801.h>
 
 #define NUM_LEDS 64
 
@@ -29,7 +28,8 @@ uint16_t r_i, r_j;  //rainbow function globals
 boolean sizeRead = false;
 boolean statLED = false;
 uint8_t statLEDPin = 12;  
-uint8_t mhv_gammaValues2[256];
+uint8_t mhv_gammaValues[256];
+uint8_t ECCByte = 0;
 Adafruit_WS2801 strip = Adafruit_WS2801(NUM_LEDS);
 
 #define PIN 2
@@ -38,7 +38,7 @@ void setup()
 {
   for(uint16_t rep=0;rep<256;rep++)
   {
-    mhv_gammaValues2[rep] = mhv_calculatedGammaCorrect(rep);
+    mhv_gammaValues[rep] = mhv_calculatedGammaCorrect(rep);
   }
   
   Serial.begin(115200);
@@ -235,42 +235,8 @@ uint8_t mhv_calculatedGammaCorrect(uint8_t value)
 {
   return (uint8_t) round(255 * pow((float)value / 256, 3.0));
 }
-const uint8_t mhv_gammaValues[] PROGMEM = {
-         0,              0,              0,              0,              0,              0,              0,              0,
-         0,              0,              0,              0,              0,              0,              0,              0,
-         0,              0,              0,              0,              0,              0,              1,              1,
-         1,              1,              1,              1,              1,              1,              1,              1,
-         1,              2,              2,              2,              2,              2,              2,              2,
-         2,              3,              3,              3,              3,              3,              4,              4,
-         4,              4,              4,              5,              5,              5,              5,              6,
-         6,              6,              6,              7,              7,              7,              7,              8,
-         8,              8,              9,              9,              9,              10,             10,             10,
-         11,             11,             12,             12,             12,             13,             13,             14,
-         14,             15,             15,             15,             16,             16,             17,             17,
-         18,             18,             19,             19,             20,             20,             21,             22,
-         22,             23,             23,             24,             25,             25,             26,             26,
-         27,             28,             28,             29,             30,             30,             31,             32,
-         33,             33,             34,             35,             36,             36,             37,             38,
-         39,             40,             40,             41,             42,             43,             44,             45,
-         46,             46,             47,             48,             49,             50,             51,             52,
-         53,             54,             55,             56,             57,             58,             59,             60,
-         61,             62,             63,             64,             65,             67,             68,             69,
-         70,             71,             72,             73,             75,             76,             77,             78,
-         80,             81,             82,             83,             85,             86,             87,             89,
-         90,             91,             93,             94,             95,             97,             98,             99,
-         101,    102,    104,    105,    107,    108,    110,    111,
-         113,    114,    116,    117,    119,    121,    122,    124,
-         125,    127,    129,    130,    132,    134,    135,    137,
-         139,    141,    142,    144,    146,    148,    150,    151,
-         153,    155,    157,    159,    161,    163,    165,    166,
-         168,    170,    172,    174,    176,    178,    180,    182,
-         184,    186,    189,    191,    193,    195,    197,    199,
-         201,    204,    206,    208,    210,    212,    215,    217,
-         219,    221,    224,    226,    228,    231,    233,    235,
-         238,    240,    243,    245,    248,    250,    253,    255
-};
 
 inline uint8_t mhv_precalculatedGammaCorrect(uint8_t value) {
            //return pgm_read_byte(mhv_gammaValues + value);
-           return mhv_gammaValues2[value];
+           return mhv_gammaValues[value];
 }
